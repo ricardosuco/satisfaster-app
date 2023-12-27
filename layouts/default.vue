56c6b72ab577a71aef7b9bc3de642930c7921613
Ricardo Avelino <rick58@ymail.com>
2023-12-27 18:26:20 -0300

    feat: monta elementos do layout

---
+++ layouts/default.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="bg-page">
+    <Header />
+    <div class="main-layout flex column">
+      <Container>
+        <NuxtPage />
+      </Container>
+      <Footer />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "default",
+};
+</script>
+
+<style lang="scss" scoped>
+.main-layout {
+  padding: 0 3rem;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  
+}
+.bg-page {
+  background-color: #f3f9fe;
+}
+</style>
