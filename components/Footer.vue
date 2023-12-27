0b07a4a3870e0153d1cd99949a40d33568087325
Ricardo Avelino <rick58@ymail.com>
2023-12-27 18:31:30 -0300

    feat: add footer

---
+++ components/Footer.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="row text-center q-mt-md">
+    <div class="col-xs-12">
+      <q-separator />
+    </div>
+    <div class="col-xs-12">Footer</div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {};
+</script>
+
+<style scoped>
+.footer {
+
+}
+</style>
