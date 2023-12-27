3aefb5938af5a3b211d5a33b2744683ca2a7b837
Ricardo Avelino <rick58@ymail.com>
2023-12-27 18:25:14 -0300

    feat: cria pagina principal

---
+++ components/Container.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <slot />
+  </div>
+</template>
+
+<script lang="ts">
+export default {};
+</script>
+
+<style lang="scss" scoped>
+.container {
+  margin: 0 auto;
+  max-width: 1070px;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="q-my-lg flex justify-center" style="gap: 20px">
+      <DrinkCard v-for="i in 15" :key="i" />
+    </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "Index",
+
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss">
+</style>
