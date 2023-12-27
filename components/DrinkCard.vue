70baeb2e84fd6f116c64f0b1e93c46f2e616757c
Ricardo Avelino <rick58@ymail.com>
2023-12-27 18:24:35 -0300

    feat: add drinkcard

---
+++ components/DrinkCard.vue
@@ -0,0 +1,24 @@
+<template>
+  <q-card class="drink-card border-radius" bordered flat>
+    <img src="https://www.thecocktaildb.com/images/ingredients/gin-Small.png" />
+    <q-card-section class="flex column items-center">
+      <div>
+        <p class="text-bold text-primary">Drink</p>
+      </div>
+    </q-card-section>
+      <q-separator />
+      <q-card-actions vertical>
+        <q-btn  label="Visualizar detalhes" text-color="accent" no-caps outline flat />
+      </q-card-actions>
+  </q-card>
+</template>
+
+<script lang="ts">
+export default {};
+</script>
+
+<style scoped>
+.drink-card {
+  width: 220px;
+}
+</style>
