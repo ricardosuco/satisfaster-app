3a65b1e6d2903e6ce96af577a8172fb61c1408f0
Ricardo Avelino <rick58@ymail.com>
2024-01-17 22:04:48 -0300

    test: add tests unitarios

--- pages/index.spec.ts
+++ pages/index.spec.ts
@@ -3,13 +3,23 @@ import { mount } from '@vue/test-utils'
 import { Quasar } from 'quasar'
 import index from './index.vue'
 
-
 describe('Index component', () => {
-    const indexMount = () => {
+    const drink = {
+        id: 1,
+        name: 'Coca Cola',
+        instructions: 'Mix it all together',
+        image: 'https://www.thecocktaildb.com/images/media/drink/qyyvtu1468878544.jpg',
+        category: 'Ordinary Drink',
+    }
+    const indexMount = (options?: any) => {
         const wrapper = mount(index, {
             global: {
-                plugins: [Quasar]
+                plugins: [Quasar],
+                stubs: {
+                    DrinkCard: true
+                }
             },
+            ...options
         })
         return wrapper
     }
@@ -17,5 +27,30 @@ describe('Index component', () => {
         const wrapper = indexMount()
         expect(wrapper.vm).toBeDefined()
 
+    })
+
+    it('Should show 5x DrinkCard', async () => {      
+        const drinks = []
+        for (let i = 0; i < 5; i++) {
+            drinks.push({...drink, id: i})
+        }
+        const wrapper = indexMount({props: {drinks}})
+        const cards = wrapper.findAllComponents({name: 'DrinkCard'})
+
+        expect(cards.length).toBe(5)
+    })
+
+    it('Should not show DrinkCard when no drinks are passed', async () => {
+        const wrapper = indexMount()
+        const cards = wrapper.findAllComponents({name: 'DrinkCard'})
+
+        expect(cards.length).toBe(0)
+    })
+
+    it('Should show "Nenhum drink encontrado" when no drinks are passed', async () => {      
+        const wrapper = indexMount({props: {drinks: [], noDrinks: true}})
+        const foundEl = wrapper.find('[data-testid="no-drinks"]')
+        console.log(foundEl.text())
+        expect(foundEl.text()).toContain('Nenhum drink encontrado')
     })
 })
