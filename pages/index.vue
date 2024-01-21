40b54738f13c90e816a74beda906fe0db616deb2
Ricardo Avelino <rick58@ymail.com>
2024-01-21 18:27:42 -0300

    feat: add chip com categoria atual

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,16 @@
 <template>
-  <div v-if="!noDrinks" class="q-my-lg flex justify-center" style="gap: 20px">
-    <DrinkCard :drink="drink" v-for="drink in drinks" :key="drink.id" />
+  <div v-if="!noDrinks">
+    <div style="gap: 20px" class="q-my-lg flex justify-center">
+      <DrinkCard :drink="drink" v-for="drink in drinks" :key="drink.id" />
+    </div>
   </div>
   <div v-else data-testid="no-drinks">
     <div class="flex justify-center q-mt-lg">
       <q-icon name="sentiment_very_dissatisfied" size="100px" color="primary" />
     </div>
-    <div  class="text-center text-h5 text-bold text-primary">Nenhum drink encontrado</div>
+    <div class="text-center text-h5 text-bold text-primary">
+      Nenhum drink encontrado
+    </div>
   </div>
 </template>
 
@@ -22,7 +26,7 @@ export default {
     drinks: Array as PropType<IDrink[]>,
     noDrinks: Boolean,
   },
-}
+};
 </script>
 
 <style lang="scss">
