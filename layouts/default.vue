2056039afc997365374f35780fc146cbcc747d88
Ricardo Avelino <rick58@ymail.com>
2024-01-21 18:27:02 -0300

    fix: ajusta request e paginação

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,6 +3,9 @@
     <Header @search="handleSearch($event)" />
     <div class="main-layout flex column">
       <Container>
+        <div class="q-mt-md flex justify-center">
+          <q-chip class="text-weight-bold q-pa-md" icon="local_bar" text-color="white" color="secondary">{{ filter.category ? filter.category : 'Todos' }}</q-chip>
+        </div>
         <q-infinite-scroll @load="onLoad" :offset="250">
           <NuxtPage :drinks="drinks" :noDrinks="noDrinks" />
           <template v-slot:loading>
@@ -15,9 +18,6 @@
             </div>
           </template>
         </q-infinite-scroll>
-        {{ pagination.total }}
-        {{ drinks.length }}
-        {{ showSpinner }}
       </Container>
       <Footer />
     </div>
@@ -26,7 +26,7 @@
 
 <script lang="ts">
 import { defineNuxtComponent } from "nuxt/app";
-import { type IDrink } from "@/models";
+import { type IDrink, type Filter } from "@/models";
 
 export default defineNuxtComponent({
   name: "default",
@@ -34,12 +34,13 @@ export default defineNuxtComponent({
     return {
       drinks: [] as PropType<IDrink[]>,
       pagination: {
-        current_page: null,
+        current_page: 0,
         last_page: null,
-        next_page_url: "/?page=1",
-        total: 0,
+        next_page_url: null,
+        previous_page_url: null,
+        total: null,
       },
-      filter: {},
+      filter: {} as Filter,
       showSpinner: true,
       noDrinks: false,
     };
@@ -47,32 +48,19 @@ export default defineNuxtComponent({
 
   computed: {
     page() {
-      // return this.pagination?.total !== this.drinks.length
-      //   ? `/?page=${this.pagination?.current_page + 1}`
-      //   : `/?page=${this.pagination?.current_page}`;
-
-      if (!this.pagination.current_page && !this.pagination.last_page)
-      return '/?page=1'
-      else if (this.pagination?.total !== this.drinks.length) 
-      return `/?page=${this.pagination?.current_page + 1}`
-      else return `/?page=${this.pagination?.current_page}`
+      if (!this.pagination.previous_page_url && !this.pagination.next_page_url)
+        return `/?page=1`;
+      else return `/?page=${this.pagination?.current_page + 1}`;
     },
   },
 
   methods: {
-    async fetchData(resetPagination = false) {
+    async fetchData() {
       try {
         this.showSpinner = true;
         const query = {};
-        if (this.filter?.category) query.category = this.filter.category;
         if (this.filter?.name) query.name = this.filter.name;
-        if (resetPagination) {
-          this.drinks.length = 0
-          this.pagination.total = 0
-          this.pagination.current_page = null
-          this.pagination.last_page = null
-          this.pagination.next_page_url = "/?page=1"
-        }
+        if (this.filter?.category) query.category = this.filter.category;
         const { data, error } = await useFetchDrinks(
           `/drinks${this.page}&rowsPerPage=50`,
           { query }
@@ -83,27 +71,29 @@ export default defineNuxtComponent({
         console.error(error);
       } finally {
         this.showSpinner = false;
-        this.drinks.length ? this.noDrinks = false : this.noDrinks = true
+        this.drinks.length ? (this.noDrinks = false) : (this.noDrinks = true);
       }
     },
 
-    async handleSearch(filter: any) {
+    resetPagination() {
+      this.pagination.total = null;
+      this.pagination.current_page = 0;
+      this.pagination.previous_page_url = null;
+      this.pagination.next_page_url = null;
+    },
+
+    async handleSearch(filter: Filter) {
       this.filter = filter;
       this.drinks = [];
-      await this.fetchData(true);
+      this.resetPagination();
+      await this.fetchData();
     },
 
     async onLoad(index: number, done: () => void) {
-      console.log(this.pagination?.next_page_url)
-      if (!this.pagination?.next_page_url) return;
-      if (this.filter?.name || this.filter?.category) await this.fetchData(true);
-      else await this.fetchData();
+      if (this.drinks.length === this.pagination.total) return done();
+      await this.fetchData();
       done();
     },
-  },
-
-  async created() {
-    await this.fetchData();
   },
 });
 </script>
